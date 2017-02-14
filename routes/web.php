6d2b9d2f2d4e0d48298f70ab67e7b82cc2f457bc
Mazeyar Rezaei <mazeyarr@gmail.com>
2017-02-15 00:34:55 +0100

    optimize fixes

--- routes/web.php
+++ routes/web.php
@@ -16,41 +16,48 @@ Route::group(['middleware' => 'auth'], function () {
         return redirect()->route('dashboard');
     });
 
-    /* Colleges Page */
-    Route::get('/dashboard/college', 'HomeController@getColleges')->name('colleges');
+    Route::group(['prefix' => 'dashboard'], function () {
 
-    Route::get('/dashboard/college/view/{id}', 'HomeController@getCollegeTimeline')->name('view_colleges');
-    Route::get('/dashboard/college/change/{id}', 'HomeController@getChangeColleges')->name('change_colleges');
-    Route::post('/dashboard/college/change/{id}/check', 'CollegeController@postChangeCollege')->name('save_change_colleges');
-    Route::get('/dashboard/college/change/{id}/check/selection/{collegename?}/{collegelocation?}', 'CollegeController@getChangeAssessorsSelection')->name('change_college_assessors');
+        /* Colleges Page */
+        Route::get('college', 'HomeController@getColleges')->name('colleges');
+        Route::group(['prefix' => 'college'], function () {
+            Route::get('view/{id}', 'HomeController@getCollegeTimeline')->name('view_colleges');
+            Route::get('change/{id}', 'HomeController@getChangeColleges')->name('change_colleges');
+            Route::post('change/{id}/check', 'CollegeController@postChangeCollege')->name('save_change_colleges');
+            Route::get('change/{id}/check/selection/{collegename?}/{collegelocation?}', 'CollegeController@getChangeAssessorsSelection')->name('change_college_assessors');
 
-    Route::get('/dashboard/college/save/assessor/{id}/{collegeid}', 'FunctionalController@ajaxSaveAssessorToCollege')->name('ajax_save_college_by_selection');
-    Route::get('/dashboard/college/save/{id}/{name}/{location}/{team}', 'FunctionalController@ajaxSaveCollege')->name('ajax_save_college');
-
-
-    /* Teamleaders Page */
-    Route::get('/dashboard/teamleaders', 'HomeController@getTeamleaders')->name('teamleaders');
-
-    Route::get('/dashboard/teamleader/view/{id}', 'HomeController@getTeamleaderTimeline')->name('view_teamleaders');
-    Route::get('/dashboard/teamleader/change/{id}', 'HomeController@getChangeTeamleader')->name('change_teamleaders');
-    Route::post('/dashboard/teamleader/change/{id}/check', 'TeamleaderController@postChangeTeamleader')->name('save_change_teamleaders');
-
-    Route::get('/dashboard/teamleader/save/{id}/{name}/{team}', 'FunctionalController@ajaxSaveTeamleader')->name('ajax_save_teamleader');
+            Route::get('save/assessor/{id}/{collegeid}', 'FunctionalController@ajaxSaveAssessorToCollege')->name('ajax_save_college_by_selection');
+            Route::get('save/{id}/{name}/{location}/{team}', 'FunctionalController@ajaxSaveCollege')->name('ajax_save_college');
+        });
 
 
-    /* Assessors Page */
-    Route::get('/dashboard/assessors', 'HomeController@getAssessors')->name('assessors');
+        /* Teamleaders Page */
+        Route::get('teamleaders', 'HomeController@getTeamleaders')->name('teamleaders');
+        Route::group(['prefix' => 'teamleader'], function () {
+            Route::get('view/{id}', 'HomeController@getTeamleaderTimeline')->name('view_teamleaders');
+            Route::get('change/{id}', 'HomeController@getChangeTeamleader')->name('change_teamleaders');
+            Route::post('change/{id}/check', 'TeamleaderController@postChangeTeamleader')->name('save_change_teamleaders');
 
-    Route::get('/dashboard/assessor/profile/{id}', 'HomeController@getAssessorProfile')->name('view_assessor_profiel');
-    Route::get('/dashboard/assessor/view/{id}', 'HomeController@getAssessorTimeline')->name('view_assessor');
-    Route::get('/dashboard/assessor/change/{id}', 'HomeController@getChangeAssessor')->name('change_assessor');
-    Route::post('/dashboard/assessor/change/{id}/check', 'AssessorController@postChangeAssessor')->name('save_change_assessor');
-    Route::get('/dashboard/assessor/add/', 'HomeController@getAddAssessor')->name('add_assessor');
-    Route::get('/dashboard/assessor/add/manual', 'HomeController@getAddAssessorManual')->name('add_assessor_manual');
-    Route::post('/dashboard/assessor/add/manual/save/{count?}', 'AssessorController@postAddAssessorManual')->name('add_assessor_manual_save');
-    Route::get('/dashboard/assessor/add/automatic', 'HomeController@getAddAssessorAutomatic')->name('add_assessor_automatic');
-    Route::get('/dashboard/assessor/excel/layout/download', 'FunctionalController@downloadExcelAssessorLayout')->name('download_excel_assessor_layout');
-    Route::post('/dashboard/assessor/add/automatic/save', 'AssessorController@postAddAssessorAutomatic')->name('add_assessor_automatic_save');
+            Route::get('save/{id}/{name}/{team}', 'FunctionalController@ajaxSaveTeamleader')->name('ajax_save_teamleader');
+        });
+
+
+        /* Assessors Page */
+        Route::get('assessors', 'HomeController@getAssessors')->name('assessors');
+        Route::group(['prefix' => 'assessor'], function () {
+            Route::get('profile/{id}', 'HomeController@getAssessorProfile')->name('view_assessor_profiel');
+            Route::get('view/{id}', 'HomeController@getAssessorTimeline')->name('view_assessor');
+            Route::get('change/{id}', 'HomeController@getChangeAssessor')->name('change_assessor');
+            Route::post('change/{id}/check', 'AssessorController@postChangeAssessor')->name('save_change_assessor');
+            Route::get('add/', 'HomeController@getAddAssessor')->name('add_assessor');
+            Route::get('add/manual', 'HomeController@getAddAssessorManual')->name('add_assessor_manual');
+            Route::post('add/manual/save/{count?}', 'AssessorController@postAddAssessorManual')->name('add_assessor_manual_save');
+            Route::get('add/automatic', 'HomeController@getAddAssessorAutomatic')->name('add_assessor_automatic');
+            Route::get('excel/layout/download', 'FunctionalController@downloadExcelAssessorLayout')->name('download_excel_assessor_layout');
+            Route::post('add/automatic/save', 'AssessorController@postAddAssessorAutomatic')->name('add_assessor_automatic_save');
+        });
+
+    });
 
     /* Dashboard Page */
     Route::get('/dashboard/{year?}', 'HomeController@getDashboard')->name('dashboard');
