4965d9eb9433a2a77d0231ccbfab5ecb2e601f83
Mazeyar Rezaei <mazeyarr@gmail.com>
2017-03-29 15:40:26 +0200

    Documenting code (ALL) part 1/4

--- app/Assessors.php
+++ app/Assessors.php
@@ -4,70 +4,133 @@ namespace App;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Assessors extends Model
 {
+    /**
+     * @param null $id
+     * @return if $id was not given it will return Collection of all Assessors
+     * @else
+     * @return Collection of Assessor
+     */
     public static function getAssessors($id=null){
+        # SECTOR 1
         $assessors = array();
 
+        # SECTOR 2
         if (self::all()->isEmpty()){
             return null;
         }
 
-        if (!is_null($id)) {
-            $assessor = self::find($id);
+        # SECTOR 3
+        if (!empty($id)) {
+            # SECTOR 3.1
+            $assessor = self::find($id); /** @returns Collection of $this Model */
 
-            if (!is_null($assessor->fk_college)) {
-                $college = College::find($assessor->fk_college);
-                $assessor->fk_college = $college;
+            # SECTOR 3.2
+            /** If the college #id is not a faulty one */
+            if (!empty($assessor->fk_college)) {
+                $college = College::find($assessor->fk_college); /** $college will become a Collection */
+                $assessor->fk_college = $college; /** replace the Collection collum "fk_college" with the bounded Collection */
             }
 
-            if (!is_null($assessor->fk_teamleader)) {
-                $teamleader = Teamleaders::find($assessor->fk_teamleader)->name;
+            # SECTOR 3.3
+            /** If the teamleader #id is not a faulty one */
+            if (!empty($assessor->fk_teamleader)) {
+                $teamleader = Teamleaders::find($assessor->fk_teamleader)->name; /** Place name from Teamleader in $teamleader */
 
+                # SECTOR 3.3.1
+                /** if the Teamleader could be found */
                 if (!empty($teamleader)) {
-                    $assessor->fk_teamleader = $teamleader;
+                    $assessor->fk_teamleader = $teamleader; /** replace the Collection collum "fk_teamleader" with the name of the $teamleader */
                 }
             }
 
-            $exams = Exams::find($assessor->fk_exams);
+            # SECTOR 3.4
+            $exams = Exams::find($assessor->fk_exams); /** @returns Collection of the Exam Model */
 
+            # SECTOR 3.5
+            /** if the Model returned Collection */
             if (!empty($exams)) {
-                $exams->basictraining = json_decode($exams->basictraining);
-                $assessor->fk_exams = $exams;
+                $exams->basictraining = json_decode($exams->basictraining); /** replace Collection collum "basictraining" a json decoded version */
+                $assessor->fk_exams = $exams; /** replace this Collection collum from this $assessor to the Collection of $exams */
             }
-
             return $assessor;
         }
 
-        foreach (self::all() as $assessor) {
+        /** If $id was not given as a parameter of this __FUNCTION__ */
 
+        # SECTIOR 4
+        /** Loop trough all the Assessors */
+        foreach (self::all() as $assessor) {
+            # SECTOR 4.1
+            /** Get college of the assessor by using the bounded foreign key */
             $college = College::find($assessor->fk_college);
+
+            # SECTOR 4.2
+            /** replace Collection "fk_college" field with a Collection of the college
+             * @note that if college is not found it wil return null
+             */
             $assessor->fk_college = $college;
 
+            # SECTOR 4.3
+            /** Get teamleader of the assessor by using the bounded foreign key */
             $teamleader = Teamleaders::find($assessor->fk_teamleader);
 
+            # SECTOR 4.4
+            /** if the $teamleader is not null */
             if (!empty($teamleader)) {
+                /** replace Assessor Collection "fk_teamleader" field to the teamleader name */
                 $assessor->fk_teamleader = $teamleader->name;
             }
 
+            # SECTOR 4.5
+            /** find the Exams data that belongs to this assessor
+             * @note that every assessor has exam data
+             */
             $exams = Exams::find($assessor->fk_exams);
 
+            # SECTOR 4.6
+            /** if $exams does not return a null value */
             if (!empty($exams)) {
+                # SECTOR 4.6.1
+                /** replace the $exams(Collection) "basictraining" field to a json decoded version*/
                 $exams->basictraining = json_decode($exams->basictraining);
+
+                # SECTOR 4.6.2
+                /** replace the Assessor(Collection) "fk_exams" to the new temporary $exams Collection*/
                 $assessor->fk_exams = $exams;
             }
-            $assessors[] = $assessor;
+            $assessors[] = $assessor; /** add new (temp) made assessor to array with all the others */
         }
+
         return $assessors;
     }
 
+    /**
+     * @param $id "ID of an Assessor"
+     * @param $date "(DateTime 'Y-m-d H:i:s') Date of which the maintenance will start"
+     * @param $place "(bool) if u want to place assessor in group"
+     * @return bool "True or False"
+     */
     public static function ScheduledForMaintenance($id, $date, $place) {
+        /** @var  $assessor Collection */
         $assessor = self::find($id);
+
+        /** if $assessor Collection is null */
         if (empty($assessor)) {
             return false;
         }
+
+        /**
+         * @function: whatKindOfMaintenance($id)
+         * @param $id ID of the Assessor
+         * @returns string
+         */
         $type = Maintenance::whatKindOfMaintenance($id);
+
+        /** If assessor needs to be placed in maintenance mode */
         if ($place) {
             if ($type == "maintenance"){
                 $exam = Exams::find($assessor->fk_exams);
